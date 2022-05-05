0914619f7d5e616d2f0266938ebadd5f68f3a255
luccaloopz <luccamartins2012@gmail.com>
2022-05-04 21:10:36 -0700

    wrote the get route for Products to get a product by a specific id.

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -27,9 +27,31 @@ router.get('/', async (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [
+        {
+          model: Category
+        },
+        {
+          model: Tag
+        }
+      ]
+    });
+
+    if (productData === null) {
+      res.status(404).json({ message: 'No traveller found with this id!' });
+      return;
+    } else {
+      res.status(200).json(productData);
+    };
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
